4cc79f4b2792c3f1b8557daa237875263299af3f
simoneluca00 <si.zaino@libero.it>
2022-05-05 20:15:30 +0200

    Additional conditions for inputs

--- PHP snack 2/index.php
+++ PHP snack 2/index.php
@@ -13,19 +13,6 @@ $userName = (isset($_GET['name'])) ? $_GET['name'] : '';
 $userMail = (isset($_GET['mail'])) ? $_GET['mail'] : '';
 $userAge = (isset($_GET['age'])) ? $_GET['age'] : '';
 
-/* 
-
-    if (empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])) {
-        echo "Compila tutti i campi:";
-    } else if (is_numeric($userAge)  && strlen($userName) > 3  && strpos($userMail, '@') && strpos($userMail, '.')) {
-        echo "Accesso riuscito";
-    } else {
-        echo "Accesso negato";
-    }
-
-*/
-
-
 
 ?>
 
@@ -35,63 +22,106 @@ $userAge = (isset($_GET['age'])) ? $_GET['age'] : '';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP snacks blocco 1</title>
+    <title>PHP snacks blocco 1 (snack 2) </title>
 
     <style>
+        div.container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
         form {
-            width: 10%;
-            margin: 0 auto;
+            width: 40%;
+        }
+
+        form p {
+            font-size: 0.9em;
+            color: red;
+            margin: 0;
+        }
+
+        label,
+        button {
+            margin-top: 20px;
         }
 
         label,
         input {
             display: block;
+            width: 100%;
         }
 
-        input {
-            margin-bottom: 20px;
+        button {
+            text-align: center;
+            width: 100%;
+            height: 40px;
         }
 
         h2 {
             text-align: center;
             margin-bottom: 30px;
+            width: 100%;
         }
 
     </style>
 </head>
 <body>
-
+    <div class="container">
+        
+    <!-- NOTE se uno dei tre input è vuoto stampare: "compila tutti i campi" -->
         <?php if (empty($_GET['name']) || empty($_GET['mail']) || empty($_GET['age'])) { ?>
             <h2>
                 <strong>
                     <?php echo "Compila tutti i campi:"; ?>
                 </strong>
             </h2>
+    <!-- NOTE se sono soddisfatte tutte le condizioni stampare: "accesso garantito" -->
         <?php } else if (is_numeric($userAge)  && strlen($userName) > 3  && strpos($userMail, '@') && strpos($userMail, '.')) { ?>
             <h2 style="color: green;">
                 <?php echo "Accesso riuscito"; ?>
             </h2>
         <?php } else { ?>
+    <!-- NOTE se qualche condizione non è soddisfatta stampare "accesso negato" -->
             <h2 style="color: red;">
                 <?php echo "Accesso negato"; ?>
             </h2>
         <?php } ?>
 
-    <form method="get">
-        <label for="name">Nome:</label>
-        <input type="text" name="name" placeholder="Inserisci il tuo nome">
+        <form method="get">
+            <label for="name">Nome:</label>
+            <input type="text" name="name" placeholder="Inserisci il tuo nome">
 
-        <label for="mail">Mail:</label>
-        <input type="text" name="mail" placeholder="Inserisci la tua mail">
+            <!-- NOTE condizione per stampare a schermo l'errore fatto dall'utente (NOME) -->
+            <?php if (!empty($_GET['name']) && strlen($userName) < 3) { ?>
+                <p>Min. 3 caratteri</p>
+            <?php } ?>
 
-        <label for="age">Età:</label>
-        <input type="text" name="age" placeholder="Inserisci la tua età">
 
-        <button type="submit">Invia</button>
-    </form>
+            <label for="mail">Mail:</label>
+            <input type="text" name="mail" placeholder="Inserisci la tua mail">
 
-    <div>
-        <?php ?>
+            <!-- NOTE condizione per stampare a schermo l'errore fatto dall'utente (MAIL DOT) -->
+            <?php if (!empty($_GET['mail'])) { 
+                if (strpos($userMail, '.') == false || strpos($userMail, '@') == false) {
+            ?>
+            <p>La mail deve contenere il "." (dot) e la "@" (at sign)</p>
+
+            <?php  
+                }
+            }  
+            ?>
+
+
+            <label for="age">Età:</label>
+            <input type="text" name="age" placeholder="Inserisci la tua età">
+
+            <!-- NOTE condizione per stampare a schermo l'errore fatto dall'utente (ETA') -->
+            <?php if (!empty($_GET['age']) && !is_numeric($userAge)) { ?>
+                <p>Inserisci un numero</p>
+            <?php } ?>
+
+            <button type="submit">Invia</button>
+        </form>
 
     </div>
     
